64094891fc6364bb5c0cfd7810f4853675007ebf
Saket Verma <stverma128@gmail.com>
2025-06-27 16:27:11 +0530

    Delete All C Program from vocational training directory

--- All C Program from vocational training/enenorodd.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-main()
-{
-int n;
-printf("enter n value\n");
-scanf("%d",&n);
- (n%2==0)?printf("%d is even number\n",n):printf("%d is odd number\n",n);
-}
--- All C Program from vocational training/pointer.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int a,b,c = 10;
-    int *p;
-    a = 4 *(c + 5);
-    p = &c;
-    b = 4 * (*p + 5);
-    printf("a = %d and b = %d",a , b);
-}
